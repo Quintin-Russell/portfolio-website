4c799dd00d0c3867ebcd3288dd1319ff630192cf
Quintin Russell <quinn.j.russell@gmail.com>
2022-04-19 22:49:28 +0000

    installed jquery

--- portfolio/client/app.jsx
+++ portfolio/client/app.jsx
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+import parseRoute from './parse-route'
 
 export default function App(props) {
+  const [hash, setHash] = useState(parseRoute(window.location.hash))
+  console.log('hash:', hash)
+  console.log('$(window):', $(window))
+
+
+
   return (
     <>
     <h1>What's up</h1>
