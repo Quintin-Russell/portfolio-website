766cb64117e5431dcc60f99c56d6d1b673ab7a74
Quintin Russell <quinn.j.russell@gmail.com>
2022-04-20 15:02:46 -0700

    added React 18 syntax to index.jsx

--- portfolio/client/index.jsx
+++ portfolio/client/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import $ from 'jquery'
+
 import App from './app'
 
-ReactDOM.render(
-  <App />,
-  document.querySelector('#root')
-);
+const root = ReactDOM.createRoot($('#root')[0])
+root.render(<App />)
