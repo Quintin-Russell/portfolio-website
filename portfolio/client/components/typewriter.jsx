bb4c8dc61fe14c95243608727a818efd8f413cd1
Quintin Russell <quinn.j.russell@gmail.com>
2022-04-28 15:57:11 -0700

    refactoring typewriter to include Multiple lines

--- portfolio/client/components/typewriter.jsx
+++ portfolio/client/components/typewriter.jsx
@@ -7,56 +7,62 @@ import { jsx, css } from '@emotion/react'
 import style from '../style/style'
 
 export default function Typewriter(props) {
- const [cursor, toggleCursor] = useState(true);
- let [counter, setCounter] = useState(0);
- const [dispString, pushDispString] = useReducer(
-  (dispString, newVal) => [...dispString, newVal],
-  [])
-
- const tarStringSplit = (props.tarString)
-    ? props.tarString.split(" ")
-    : null
-
-
- useEffect(()=> {
-  const intCursor = setInterval(() => {
-    toggleCursor(cursor => !cursor)
-    if (cursor && (counter !== tarStringSplit.length)) {
-      const nextWord = (props.bold.includes(tarStringSplit[counter]))
-        ? (
-          {
-            word: tarStringSplit[counter],
-            bold: 'bold'
-          }
-        )
-        : (
-          {
-            word: tarStringSplit[counter],
-            bold: 'normal'
-          }
-        )
-      pushDispString(nextWord)
-      setCounter(counter++)
-    }
-
-  }, 200);
-
-  return () => clearInterval(intCursor)
- }, [])
-
- const font = (props.font) ? style[props.font] : null
-
- return (
-   <>
-       <span></span>
-       <p>
-       {dispString.map((x) => <>
-         <span key={x.word} css={(x.bold === 'bold') ? [font, style.typewriterLandingPage, style.bold] : [font, style.typewriterLandingPage]}>{x.word} </span>
-       <span> </span>
-       </>)}
-         <span css={[font, style.typewriterLandingPage]}>{(cursor) ?"|" :""}</span>
-        </p>
-     <span></span>
-   </>
- )
+  for (const item of props.items) {
+    console.log('item in typewriter:', item)
+  }
 }
+
+// export default function Typewriter(props) {
+//  const [cursor, toggleCursor] = useState(true);
+//  let [counter, setCounter] = useState(0);
+//  const [dispString, pushDispString] = useReducer(
+//   (dispString, newVal) => [...dispString, newVal],
+//   [])
+
+//  const tarStringSplit = (props.tarString)
+//     ? props.tarString.split(" ")
+//     : null
+
+
+//  useEffect(()=> {
+//   const intCursor = setInterval(() => {
+//     toggleCursor(cursor => !cursor)
+//     if (cursor && (counter !== tarStringSplit.length)) {
+//       const nextWord = (props.bold.includes(tarStringSplit[counter]))
+//         ? (
+//           {
+//             word: tarStringSplit[counter],
+//             bold: 'bold'
+//           }
+//         )
+//         : (
+//           {
+//             word: tarStringSplit[counter],
+//             bold: 'normal'
+//           }
+//         )
+//       pushDispString(nextWord)
+//       setCounter(counter++)
+//     }
+
+//   }, 200);
+
+//   return () => clearInterval(intCursor)
+//  }, [])
+
+//  const font = (props.font) ? style[props.font] : null
+
+//  return (
+//    <>
+//        <span></span>
+//        <p>
+//        {dispString.map((x) => <>
+//          <span key={x.word} css={(x.bold === 'bold') ? [font, style.typewriterLandingPage, style.bold] : [font, style.typewriterLandingPage]}>{x.word} </span>
+//        <span> </span>
+//        </>)}
+//          <span css={[font, style.typewriterLandingPage]}>{(cursor) ?"|" :""}</span>
+//         </p>
+//      <span></span>
+//    </>
+//  )
+// }
--- portfolio/client/pages/landingPage.jsx
+++ portfolio/client/pages/landingPage.jsx
@@ -10,6 +10,18 @@ import style from "../style/style";
 // ideas for continuous typewriter:
   // attribute = array object with props => typewriter reads array in order
 
+const landingPageTxt = {
+  font: 'monts',
+  tarString: 'At my core, I am a constant asker of why? and how did you do that?. On the outside, I use React.js, jQuery, Express.js, PostgreSQL, Python, and more to create web- based applications.I am actively looking for opportunities that allow me to learn everyday and improve in what I know.',
+  bold: ['why?', 'how', 'did', 'you', 'do', 'that?.', 'React.js,', 'jQuery,', 'Express.js,', 'PostgreSQL,', 'Python,']
+}
+
+const landingPageHeading = {
+  font='pfDisp'
+  tarString="I build intelligent and impactful applications that work."
+  bold = ["I", "build", "intelligent", "and", "impactful", "applications", "that", "work."]
+}
+
 export default function LandingPage() {
   return (
     <>
@@ -24,13 +36,14 @@ export default function LandingPage() {
         "
           bold={["I", "build", "intelligent", "and", "impactful", "applications", "that", "work."]}
         /> */}
-        <Typewriter
+        {/* <Typewriter
         font='monts'
         tarString="At my core, I am a constant asker of why? and how did you do that?.
                   On the outside, I use React.js, jQuery, Express.js, PostgreSQL, Python,
                   and more to create web-based applications. I am actively looking for
                   opportunities that allow me to learn everyday and improve in what I know."
-        bold={['why?', 'how', 'did', 'you', 'do', 'that?.', 'React.js,', 'jQuery,', 'Express.js,', 'PostgreSQL,', 'Python,']}/>
+        bold={['why?', 'how', 'did', 'you', 'do', 'that?.', 'React.js,', 'jQuery,', 'Express.js,', 'PostgreSQL,', 'Python,']}/> */}
+        <Typewriter items={[landingPageHeading,landingPageTxt]} />
       </ParallaxLayer>
     </>
   )
