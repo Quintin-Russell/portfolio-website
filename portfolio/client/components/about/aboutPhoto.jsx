d4128ae2fae841f07bf2c4e6759788075f16b84d
Quintin Russell <quinn.j.russell@gmail.com>
2022-05-05 16:40:44 +0000

    added aboutPhoto component

---
+++ portfolio/client/components/about/aboutPhoto.jsx
@@ -0,0 +1,12 @@
+/** @jsx jsx */
+import React from 'react'
+import { jsx, css } from '@emotion/react'
+
+import style from '../../style/style'
+import layout from '../../style/layout'
+
+export default function aboutPhoto() {
+  return (
+<></>
+  )
+}
