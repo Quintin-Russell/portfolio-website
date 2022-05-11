0fa8476d28a98c0daf8b13cab15637ede83ac742
Quintin Russell <quinn.j.russell@gmail.com>
2022-05-11 13:04:22 -0700

    index.js setup complete

--- portfolio/server/index.js
+++ portfolio/server/index.js
@@ -1,3 +1,12 @@
 require('dotenv/config');
 const express = require('express')
 const db = require('./db')
+const errorMiddleware = require('./error-middleware');
+const staticMiddleware = require('./static-middleware');
+const ClientError = require('./client-error');
+
+const app = express();
+const jsonMiddleware = express.json();
+
+app.use(staticMiddleware);
+app.use(jsonMiddlerware);
