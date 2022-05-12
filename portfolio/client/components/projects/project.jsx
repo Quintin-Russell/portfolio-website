cf234cacc245f7608eb48ae3cf85f96f3e3d011a
Quintin Russell <quinn.j.russell@gmail.com>
2022-05-12 23:02:01 +0000

    added images to projects folder

--- portfolio/client/components/projects/project.jsx
+++ portfolio/client/components/projects/project.jsx
@@ -4,6 +4,8 @@ import React, {useState,
 import { jsx, css } from '@emotion/react'
 import { ParallaxLayer } from '@react-spring/parallax'
 
+import iPhoneFrame from '../../../server/public/images/projects/iPhoneFrame.png'
+
 import style from "../../style/style";
 import layout from "../../style/layout"
 
@@ -11,7 +13,21 @@ export default function Project(props) {
   const alternate = () => {
     if (props.index % 2) {
       return (
-
+        <div css={[layout.flex, layout.width80, layout.alignC, layout.justSpbw]}>
+          <div css={[layout.flex, layout.flex66]}></div>
+          <div css={[layout.flex, layout.flex33]}>
+            <img src={iPhoneFrame} alt="iPhone-Frame" />
+          </div>
+        </div>
+      )
+    } else {
+      return (
+        <div css={[layout.flex, layout.width80, layout.alignC, layout.justSpbw]}>
+          <div css={[layout.flex, layout.flex33]}>
+            <img src={iPhoneFrame} alt="iPhone-Frame" />
+          </div>
+          <div css={[layout.flex, layout.flex66]}></div>
+        </div>
       )
     }
   }
