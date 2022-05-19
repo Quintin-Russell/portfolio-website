8c127d028b7df0ae48165da933685911f3953111
Quintin Russell <quinn.j.russell@gmail.com>
2022-05-19 00:15:08 +0000

    added react-form hook

--- portfolio/client/components/contact/contactForm.jsx
+++ portfolio/client/components/contact/contactForm.jsx
@@ -1,8 +1,6 @@
 /** @jsx jsx */
-import React, {
-  useState,
-  useEffect
-} from "react";
+import React from "react";
+import {useForm} from "react-hook-form"
 import { jsx, css } from '@emotion/react'
 import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 
@@ -13,10 +11,25 @@ import ViewSwap from "../../components/viewSwap";
 
 import style from "../../style/style";
 import layout from "../../style/layout"
-
+/*
+-obj = empty
+-goals:
+  *form element
+  *submit handler
+    -posts user info to db
+    -sends email
+    -useForm hook: https://www.youtube.com/watch?v=bU_eq8qyjic
+      *const {register, handleSubmit, errors} = useForm()
+        register: passed to inputs using "ref": ref={register}
+        handleSubmit: takes your custom "submit handler funct" as arg;
+          passed to form in onSubmit handler: onSubmit={handleSubmit(_submit handler funct_)}
+*/
 export default function ContactForm() {
+  const {register, handleSubmit, error} = useForm();
+
   return (
     <>
+
     </>
   )
 }
