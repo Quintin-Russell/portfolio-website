eedc90e5cb80a10928f3838d38d9203123c12d9c
Quintin Russell <quinn.j.russell@gmail.com>
2022-05-20 03:32:51 +0000

    commiting

--- portfolio/client/components/contact/submittedModal.jsx
+++ portfolio/client/components/contact/submittedModal.jsx
@@ -12,10 +12,10 @@ export default function SubmittedModal(props) {
   return (
     <>
     <div onClick={() => props.setSubmitted(!props.submitted)}
-    css={[layout.flex, layout.col, layout.justCent, layout.alignC,layout.width80, style.borderLight, style.textCent]}>
+    css={[layout.flex, layout.col, layout.justCent, layout.alignC,layout.width80, style.borderLight, style.textCent, style.cursor]}>
       <p css={[style.pfDisp, style.font3rem]}>Thanks for reaching out!</p>
       <p css={[style.monts, style.font2rem]}>We just sent you an email. Quintin will get in touch with you soon!</p>
-      <p css={[style.incon, hoverA]}>-Back To Form-</p>
+      <p css={[style.incon]}>-Back To Form-</p>
     </div>
     </>
   )
